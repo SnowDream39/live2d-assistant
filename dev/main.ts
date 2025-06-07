f8ded6428d406c7b71c5bf0ca41d24b5f1b27ee1
SnowDream39 <759723417@qq.com>
2025-06-07 17:05:13 +0800

    修复依赖

--- dev/main.ts
+++ dev/main.ts
@@ -4,8 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import VueWindow from '@snowdream39/vue-window'
-import 'vue-window/dist/index.css'
-import 'vue-window/dist/style.css'
+import '@snowdream39/vue-window/dist/index.css'
+import '@snowdream39/vue-window/dist/style.css'
 import 'virtual:uno.css'
 
 const app = createApp(App)
